0f4311773b47c124178d2b94a5b8af1226727071
SuviGaming <60156403+SuviGaming@users.noreply.github.com>
2020-04-13 16:42:18 -0400

    Add files via upload

---
+++ Lesson-01/Examples/index.php
@@ -0,0 +1,34 @@
+<?php
+    // Hello I'm a single line comment Looking for Love
+    /*
+        I'm a multi-line comment with MPD
+    */
+    echo "Hello everybody I'm in Derpy world";
+    $name = "Suvi Virasih";
+
+    echo $name;
+    $name = 5;
+    echo "<br>";
+    echo $name;
+
+    $a_string = "I'm a string because I have quotes around me";
+    $an_integer = 2;
+    $a_float = 2.1;
+    $a_boolean = true;
+    $result = "5" + 5; // Will result in 10 (because "5" is still 5)
+    $result = "5" . 5;
+    echo "<h1>The Result is: $result</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+</body>
+</html>
---
+++ Lesson-01/Lab/lab-01.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+
+<!-- Step 1: Create a PHP block (all following steps will need to be put in the PHP block) -->
+
+  // Step 2: Write a multi-line comment with your name, student number, and one fact about you
+
+  // Step 3: Create a variable called "str" and assign a string of text to it
+
+  // Step 4: Create a variable called "float" and a assign a decimal number to it
+
+  // Step 5: Output the value of str
+
+  // Step 6: Output the value of num
+
+  // BONUS (2 points): Output step 5 and 6 so they appear visually on separate lines when viewing them in the browser
+  
+*/
+
+/*
+    Suvi Virasih
+    200431364
+    I'm a gamer addict
+*/
+
+  $str = "I am a string of text";
+  $float = "10";
+
+  echo $str;
+  echo "<br>";
+  echo $float;
+?>
